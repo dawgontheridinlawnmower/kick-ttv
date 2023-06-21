1bcf14acc228d4e20083c56a2a14e43b1dc82c86
Cássio A. Maciel <cassiomaciel35@gmail.com>
2023-06-21 13:58:59 -0300

    Fix download & update urls

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,10 @@ export default defineConfig({
 				icon: "https://www.google.com/s2/favicons?sz=64&domain=twitch.tv",
 				namespace: "dawgonntheridinlawnmower",
 				match: ["*://twitch.tv/*", "*://www.twitch.tv/*"],
-				updateURL: "",
-				downloadURL: "",
+				updateURL:
+					"https://github.com/dawgonntheridinlawnmower/kick-ttv/releases/latest/download/kickttv.user.js",
+				downloadURL:
+					"https://github.com/dawgonntheridinlawnmower/kick-ttv/releases/latest/download/kickttv.user.js",
 			},
 		}),
 	],
